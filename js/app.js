645350e017b83e76159e7cf334d5aa27b2fc64a1
jéssica senger <jessicasbogoni1@gmail.com>
2019-06-05 21:38:41 -0300

    Define all objects and methods

--- js/app.js
+++ js/app.js
@@ -24,11 +24,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+}
 
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+
+}
+
+Player.prototype.handleInput = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
+for(i=0;i<10;i++) {
+    allEnemies.push(new Enemy);
+}
 // Place the player object in a variable called player
+var player;
+
+player = new Player;
 
 
 
